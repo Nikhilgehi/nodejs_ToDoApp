bb73d75f6e43b3e74cf1cf2c6ce7270345c16df2
nikhilgehi <nikhilgehi15@gmail.com>
2023-04-09 13:42:10 +0530

    working on problem

--- utils/dist/feature.dev.js
+++ utils/dist/feature.dev.js
@@ -19,12 +19,12 @@ var sendcookie = function sendcookie(user, res, message) {
   }, process.env.JWT_SECRET);
 
   console.log(process.env.NODE_ENV);
-  console.log(process.env.NODE_ENV === "Development");
+  console.log(process.env.NODE_ENV === "Production");
   res.status(statusCode).cookie("token", token, {
     httpOnly: true,
     maxAge: 15 * 60 * 1000,
-    sameSite: process.env.NODE_ENV === "Development" ? "lax" : "none",
-    sercure: process.env.NODE_ENV === "Development" ? false : true
+    sameSite: process.env.NODE_ENV === "Production" ? "lax" : "none",
+    sercure: process.env.NODE_ENV === "Production" ? false : true
   }).json({
     success: true,
     message: message
--- utils/feature.js
+++ utils/feature.js
@@ -3,14 +3,14 @@ import bcrypt from "bcrypt";
 export const sendcookie=(user,res,message,statusCode=200)=>{
     const token=jwt.sign({_id: user._id },process.env.JWT_SECRET)
     console.log(process.env.NODE_ENV);
-    console.log(process.env.NODE_ENV==="Development");
+    console.log(process.env.NODE_ENV==="Production");
         res
         .status(statusCode)
         .cookie("token",token,{
             httpOnly:true, 
               maxAge:15*60*1000,
-              sameSite:process.env.NODE_ENV==="Development"?"lax":"none",
-              sercure:process.env.NODE_ENV==="Development"?false:true,
+              sameSite:process.env.NODE_ENV==="Production"?"lax":"none",
+              sercure:process.env.NODE_ENV==="Production"?false:true,
          })
          .json({
             success:true,
